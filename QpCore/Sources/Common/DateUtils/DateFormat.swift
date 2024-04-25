b2664d9e7f718a6bd72bd679eda26dc648525216
Davide Giuseppe Farella <fardavide@gmail.com>
2024-04-25 22:26:55 +0200

    fix: timezone #2

--- QpCore/Sources/Common/DateUtils/DateFormat.swift
+++ QpCore/Sources/Common/DateUtils/DateFormat.swift
@@ -6,11 +6,10 @@ public extension Date {
     let formatter = switch formatter {
     case .iso8601: ISO8601DateFormatter()
     }
-    formatter.timeZone = .gmt
     return formatter.date(from: string)
   }
   
-  /// Create a `date` using given parameters
+  /// Create a `date` using given parameters and the system's current timezone
   /// - Parameters:
   ///   - year: year of the `Date`
   ///   - month: month of the `year`
@@ -18,7 +17,6 @@ public extension Date {
   ///   - hour: hour of the `day`
   ///   - minute: minute of the `hour`
   ///   - second: second of the `minute`
-  ///   - millisecond: millisecond of the `second`
   /// - Returns: Date
   static func of(
     year: Int,
@@ -26,20 +24,26 @@ public extension Date {
     day: Int,
     hour: Int = 0,
     minute: Int = 0,
-    second: Int = 0,
-    millisecond: Int = 0
+    second: Int = 0
   ) -> Date {
     assert(day > 0, "day is 1-index value")
-    let string = """
-    \(year)-\
-    \(String(format: "%02d", month.ordinal() + 1))-\
-    \(String(format: "%02d", day))T\
-    \(String(format: "%02d", hour)):\
-    \(String(format: "%02d", minute)):\
-    \(String(format: "%02d", second))+\
-    \(String(format: "%04d", millisecond))
-    """
-    return from(string, formatter: .iso8601)!
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+    dateFormatter.timeZone = TimeZone.current
+    
+    let dateString = String(
+      format: "%04d-%02d-%02dT%02d:%02d:%02d",
+      year,
+      month.ordinal() + 1,
+      day,
+      hour,
+      minute,
+      second
+    )
+    guard let date = dateFormatter.date(from: dateString) else {
+      fatalError("Invalid date format or values")
+    }
+    return date
   }
   
   func formatted(dateFormat: String) -> String {
